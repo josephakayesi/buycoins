db59881e5b58390d9d378b607a1d20ef27289e9b
Joseph Akayesi <josephakayesi@gmail.com>
2019-06-17 17:55:04 +0000

    Sticky footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 class Footer extends Component {
     render() {
         return (
-            <footer className='bottom'>
-                <p>I am a footer</p>
+            <footer className='bottom' style={{ position: 'fixed', bottom: 0, left: 0, textAlign: 'center' }}>
+                <p className='text-center'>I am a footer</p>
             </footer>
         )
     }
